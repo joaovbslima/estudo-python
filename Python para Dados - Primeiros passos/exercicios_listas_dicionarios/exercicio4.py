9201245ef188791192169af09f50060b60cc6b59
Joao Victor Lima <jaovbslima@gmail.com>
2026-09-21 22:06:33 -0300

    resolução de mais exercícios

--- Python para Dados - Primeiros passos/exercicios_listas_dicionarios/exercicio4.py
+++ Python para Dados - Primeiros passos/exercicios_listas_dicionarios/exercicio4.py
@@ -1,1 +1,9 @@
-# 4) Colete novamente 5 inteiros e imprima a lista em ordem inversa à enviada.
+#%%# 4) Colete novamente 5 inteiros e imprima a lista em ordem inversa à enviada.
+lista_num = []
+for i in range(1, 6):
+    item = int(input('Digite um número inteiro: '))
+    lista_num.append(item)
+
+print(lista_num[::-1])
+
+# %%
--- Python para Dados - Primeiros passos/exercicios_listas_dicionarios/exercicio5.py
+++ Python para Dados - Primeiros passos/exercicios_listas_dicionarios/exercicio5.py
@@ -1,1 +1,22 @@
-# 5) Faça um programa que, ao inserir um número qualquer, cria uma lista contendo todos os números primos entre 1 e o número digitado.
+# # 5) Faça um programa que, ao inserir um número qualquer, cria uma lista contendo todos os números primos entre 1 e o número digitado.
+#%%
+
+def primo(numero):
+    if numero <= 1:
+        return False
+
+    for i in range(2,numero):
+        if numero % i == 0:
+            return False
+
+    return True
+
+lista_primos = []
+
+num = int(input('Digite um número inteiro: '))
+
+for i in range (2, num + 1):
+    if primo(i):
+      lista_primos.append(i)
+      
+print(lista_primos)
